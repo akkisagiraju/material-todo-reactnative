7d57cb4e5a9bed78bc1acb8144219a5044826f78
akkisagiraju <7601150+akkisagiraju@users.noreply.github.com>
2020-02-12 09:05:25 +0530

    added todo addition

--- src/screens/Todos.js
+++ src/screens/Todos.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { FAB } from 'react-native-paper';
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView
+} from 'react-native';
+import { FAB, TextInput, Snackbar } from 'react-native-paper';
 import Header from '../components/Header';
 import globalStyles from '../styles/globalStyles';
 import TodoItems from '../components/TodoItems';
 import { uuid } from 'uuidv4';
 
 const Todos = () => {
+  const [isAddingTodo, setAddingTodo] = useState(false);
+  const [todo, setTodo] = useState('');
+  const [displaySnack, setDisplaySnack] = useState(false);
+
   const [todoList, setTodoList] = useState([
     { id: uuid(), title: 'Eat' },
     { id: uuid(), title: 'code like a maniac' },
@@ -16,15 +26,67 @@ const Todos = () => {
   const onItemDelete = id => {
     const tempList = todoList.filter(todo => todo.id !== id);
     setTodoList(tempList);
+    setDisplaySnack(true);
+  };
+
+  const startAddingTodo = () => {
+    setAddingTodo(true);
+  };
+
+  const dismissAddition = () => {
+    Keyboard.dismiss();
+    setAddingTodo(false);
+  };
+
+  const onItemAdd = () => {
+    let tempList = [...todoList];
+    tempList.push({ id: uuid(), title: todo });
+    setTodoList(tempList);
+    setAddingTodo(false);
+    setTodo('');
   };
 
   return (
     <>
-      <Header title="Add Todos" />
-      <View style={styles.container}>
-        <TodoItems todoList={todoList} onItemDelete={onItemDelete} />
-        <FAB style={styles.fab} icon="plus" label="Add Todo" />
-      </View>
+      <Header title="My Todos" />
+      <TouchableWithoutFeedback onPress={dismissAddition}>
+        <View style={styles.container}>
+          <TodoItems todoList={todoList} onItemDelete={onItemDelete} />
+          <Snackbar
+            visible={displaySnack}
+            duration={3000}
+            style={{ marginBottom: 80 }}
+            onDismiss={() => setDisplaySnack(false)}
+            action={{
+              label: 'Undo',
+              onPress: () => {
+                console.log('Undo delete');
+              }
+            }}>
+            Todo deleted
+          </Snackbar>
+          {isAddingTodo ? (
+            <KeyboardAvoidingView enabled behavior="padding">
+              <TextInput
+                placeholder="New todo"
+                value={todo}
+                onChangeText={text => setTodo(text)}
+                onEndEditing={onItemAdd}
+                // multiline={true}
+                // numberOfLines={3}
+                underlineColor="rgb(255,255,255)"
+              />
+            </KeyboardAvoidingView>
+          ) : (
+            <FAB
+              style={styles.fab}
+              icon="plus"
+              label="Add Todo"
+              onPress={startAddingTodo}
+            />
+          )}
+        </View>
+      </TouchableWithoutFeedback>
     </>
   );
 };
